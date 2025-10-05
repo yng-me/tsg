bd16635724a3be003b2d596654224d7df09e3f36
Bhas Abdulsamad <aeabdulsamad@gmail.com>
2025-10-05 15:19:41 +0800

    Added utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,23 @@
 `%||%` <- function(a, b) if (!is.null(a)) a else b
+
+is_valid_input_data <- function(x) {
+  error_message <- "data input must be a valid data frame or Arrow format."
+
+  if(is.vector(x) | is.character(x)) stop(error_message)
+  if(!(inherits(x, 'data.frame') |
+       inherits(x, 'ArrowObject') |
+       inherits(x, 'arrow_dplyr_query')
+  )
+  ) stop(error_message)
+}
+
+collect_data <- function(data, ...) {
+
+  is_valid_input_data(data)
+
+  if("ArrowObject" %in% class(data) | "arrow_dplyr_query" %in% class(data)) {
+    dplyr::collect(data)
+  } else {
+    data
+  }
+}
